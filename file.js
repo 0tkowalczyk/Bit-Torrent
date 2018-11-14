520cb52e854da1611634a51db00c789c9a442777
0tkowalczyk <42749742+0tkowalczyk@users.noreply.github.com>
2018-11-14 14:02:38 -0600

    Add files via upload

---
+++ file.js
@@ -0,0 +1,22 @@
+//type your code inside this function
+function TorrentFile(){
+
+//first function
+function numberOfPackets(fileSize)
+    return fileSize/50
+
+
+
+//second function
+function buildTorrentPacketArray(numOfPackets)
+    let list;
+    let length;
+    for (let i=0; i < length; i++){
+    list [i] = 1
+    }
+
+
+
+}
+
+//main test methods
