caef1c03b6b4713c8cdb74d1fb8f232e55ed132f
0tkowalczyk <42749742+0tkowalczyk@users.noreply.github.com>
2018-11-28 01:24:55 -0600

    Add files via upload

--- tracker.js
+++ tracker.js
@@ -5,8 +5,37 @@ function Tracker(){
   let percent = [];
 
   //implement all six functions below
+  function numOfSeeds(list){
 
+    let found = 0
+  
+    for(let i = 0; i < list.length; i++){
+      if(list [i] !== null){
+        found++
+      }
+    }
+      return found
+  }
+  function addSeeds(s){
 
-
-
+    seeds.push(s) ;
+   
+   }
+   
+   function removeSeed(s){
+   
+     for(let i = 0; i < seeds.length; i++){
+   
+        if(seeds[i] === s){
+   
+         seeds[i] = null;
+         
+         percent[i] = 0;
+       
+        }
+       }
+        
+   }
+   
+    
 }
